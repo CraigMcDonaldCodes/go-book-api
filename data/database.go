187d966dc2b259103bfe81781b758a1e0c67339e
Craig McDonald <craig@mcdonald.codes>
2022-04-09 00:10:42 +0100

    'FakeDb' implements 'Db' interface, dummy data on 'Get' method for testing

--- data/database.go
+++ data/database.go
@@ -10,6 +10,29 @@ type Db interface {
 type fakeDb struct {
 }
 
+func (db fakeDb) Get(id int64) Book {
+
+	return Book{
+		Id:        1,
+		Isbn:      "foo",
+		Title:     "bar",
+		Author:    "foo",
+		Publisher: "bar",
+	}
+}
+
+func (db fakeDb) GetAll() []Book {
+	return nil
+}
+
+func (db *fakeDb) Delete(id int64) bool {
+	return false
+}
+
+func (db *fakeDb) Update(id int64) bool {
+	return false
+}
+
 func NewFakeDb() Db {
 	return nil
 }
